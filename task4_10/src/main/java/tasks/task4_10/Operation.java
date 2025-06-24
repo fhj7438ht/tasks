812ee5f6e53bf2d40117631ef7cfbb4170b6479f
maksim <->
2025-06-24 13:58:46 +0300

    add

---
+++ task4_10/src/main/java/tasks/task4_10/Operation.java
@@ -0,0 +1,5 @@
+package tasks.task4_10;
+
+public interface Operation {
+    double getResult(double a, double b);
+}
---
+++ task4_10/src/test/java/tasks/task4_10/Task410ApplicationTests.java
@@ -0,0 +1,13 @@
+package tasks.task4_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task410ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
